78e840c2c20ffd37d2ef506670973bf501f4a779
Sebastian <romy6047@gmail.com>
2024-05-11 20:04:49 -0600

    Empty express app created

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log("Express App running on port " + PORT)
+    } else {
+        console.log("Error while running express server", error);
+    }
+}); 
